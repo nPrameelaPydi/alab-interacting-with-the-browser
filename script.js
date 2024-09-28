47452d78258cd04ffe2ce499643f653d87e3c74f
Prameela Pydi <nprameela.pydi@gmail.com>
2024-09-27 22:36:10 -0700

    modified logic

--- script.js
+++ script.js
@@ -27,17 +27,42 @@ const blurredImage = document.getElementById('blurredImage');
 blurredImage.src = currentImageObj.src;
 
 
-let currentImage = currentImageObj.name.trim().toLowerCase();
-console.log(`currentImage: ${currentImage}`);
+//let currentImage = currentImageObj.name.trim().toLowerCase();
+//console.log(`currentImage: ${currentImage}`);
 
-let userGuess = window.prompt("Guess the name of the flower");
-userGuess = userGuess.trim().toLowerCase();
-console.log(`UserGuess: ${userGuess}`);
-if (userGuess === currentImage) {
-    alert(`Correct! Its a ${currentImageObj.name}.`)
-} else {
-    alert(`Wrong guess! Try again.`);
+//let userGuess = window.prompt("Guess the name of the flower");
+//userGuess = userGuess.trim().toLowerCase();
+//console.log(`UserGuess: ${userGuess}`);
+//if (userGuess === currentImage) {
+//    alert(`Correct! Its a ${currentImageObj.name}.`)
+//} else {
+//    alert(`Wrong guess! Try again.`);
+//}
+
+let numOfTries = 10;
+let correctGuess = false;
+
+
+while (numOfTries > 0 && correctGuess != true) {
+    numOfTries -= 1;
+    let userGuess = window.prompt("Guess the name of the flower");
+    console.log(userGuess);
+    if (userGuess === null) {
+        alert(`Please enter a valid guess.`);
+    } else {
+        userGuess = userGuess.trim().toLowerCase();
+        console.log("userguess: " + userGuess);
+        let currentImage = currentImageObj.name.trim().toLowerCase();
+        console.log("current: " + currentImage)
+        if (userGuess === currentImage) {
+            alert(`Correct! Its a ${currentImageObj.name}.`)
+            correctGuess = true;
+        } else {
+            alert(`Wrong guess! Try again. You have ${numOfTries} more guesses`);
+        }
+    }
 }
+
 
 
 
